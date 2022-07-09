b83041e3c7b99e0f0c9f9231ecb9f52b706f44dc
Khaled Faour <khaledfaour2@gmail.com>
2022-07-09 12:54:01 +0300

    BACKEND: Added Users Model

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -8,7 +8,14 @@ const userSchema = new mongoose.Schema({
     last_name: {type: String, required: false},
     email: {type: String, required: false},
     password: {type: String, required: false},
-
+    profile_picture: {type: String},
+    balance: {type: Number, default: 0},
+    user_type: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user_types'
+    },
+    github_url: {type: String},
+    linkedin_url: {type: String}
 });
 
 userSchema.plugin(findOrCreate)
