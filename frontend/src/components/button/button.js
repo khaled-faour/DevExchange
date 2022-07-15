ce590053c01f4d3300642192f99df418df7ef53c
Khaled Faour <khaledfaour2@gmail.com>
2022-07-16 01:02:25 +0300

    FRONTEND: Cleaned Button Component Code

--- frontend/src/components/button/button.js
+++ frontend/src/components/button/button.js
@@ -3,10 +3,6 @@ import styles from './styles';
 
 const Button = ({ children, onClick, outlined = false , fullWidth = false}) => {
     const classes = styles();
-    const classNames = {
-        outlined: outlined? classes.outlined : classes.button,
-        fullWidth: fullWidth? classes.fullWidth : ''
-    }
     return (
         <button
             className={[outlined? classes.outlinedButton : classes.button, fullWidth? classes.fullWidth : ''].join(' ')}
