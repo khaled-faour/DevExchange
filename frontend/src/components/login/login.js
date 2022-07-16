3e1dc6fcaa8833b2f349f65ca7c6fb26e2cfd3fe
Khaled Faour <khaledfaour2@gmail.com>
2022-07-16 19:08:57 +0300

    FRONTEND: Added Login Functionality

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -4,6 +4,7 @@ import styles from './styles';
 import Button from '../button/button';
 import googleImage from '../../assets/images/Google.svg';
 import githubImage from '../../assets/images/Github.svg';
+import axios from 'axios';
 
 // Material UI
 import Divider from '@mui/material/Divider';
@@ -12,23 +13,56 @@ import Divider from '@mui/material/Divider';
 
 const Login = ()=>{
     const classes = styles();
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: ''
+    });
+    const [error, setError] = useState('');
+    const onChange = (e)=>{
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        });
+    }
+    const onSubmit = (e)=>{
+        axios.post('/auth/login', credentials)
+        .then(res=>{
+            console.log(res);
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
+
+    const googleLogin = ()=>{
+        window.location.href = 'http://localhost:4001/api/auth/google'
+    }
+
+    const githubLogin = ()=>{
+        window.location.href = 'http://localhost:4001/api/auth/github'
+    }
+
+    useEffect(()=>{
+        console.log(credentials);
+    }
+    ,[credentials])
     return (
         <div className={classes.container}>
 
 
             {/* Login By Email and Password */}
-            <Input type="text" placeholder="Email" />
-            <Input type="password" placeholder="Password" />
-            <Button fullWidth>Login</Button>
+            <Input name="email" type="text" placeholder="Email" onChange={onChange}/>
+            <Input name= "password" type="password" placeholder="Password" onChange={onChange}/>
+            <Button onClick={onSubmit} fullWidth>Login</Button>
 
             {/* <hr className={classes.hr}></hr> */}
             <Divider className={classes.divider}>or</Divider>
 
             {/* Login With Google */}
-            <img className={classes.socialButton} src={googleImage} alt="google" height={45}/>
+            <img className={classes.socialButton} src={googleImage} alt="google" height={45} onClick={googleLogin}/>
 
             {/* Login With Github */}
-            <img className={classes.socialButton} src={githubImage} alt="github" height={45}/>
+            <img className={classes.socialButton} src={githubImage} alt="github" height={45} onClick={githubLogin}/>
 
         </div>
         
