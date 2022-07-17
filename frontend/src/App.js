b048135f38afba4ef148305787db1ddd2c069311
Khaled Faour <khaledfaour2@gmail.com>
2022-07-17 16:26:21 +0300

    FRONTEND: Cleand App Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
 import React, {useEffect} from "react";
-import Home from "./pages/landingPage/landing";
 import useAuth from "./hooks/useAuth";
-import {useSelector} from "react-redux";
+import Routes from "./Routes";
 
 function App() {
   const auth = useAuth();
-  const user = useSelector(state => state.user);
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     auth.verify();
-  },[])
+  }
+  , []);
 
   return (
     <div className="App">
-      {!auth.isAuth ? <Home /> : <button onClick={auth.logout}>Logout</button>}
+      <Routes />
     </div>
   );
 }
