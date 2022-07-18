b0a6739f51caedabc54987b98035c1d5286def23
Khaled Faour <khaledfaour2@gmail.com>
2022-07-18 11:55:00 +0300

    FRONTEND: Added Container to Routes

--- frontend/src/Routes/routes.js
+++ frontend/src/Routes/routes.js
@@ -1,16 +1,19 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "../pages/home/home";
 import Navbar from "../components/navbar/navbar";
+import Container from "../components/container/container";
 
 const Router = () => {
     return(
         <>
             <Navbar/>
-            <Routes>
-                <Route path="/" element={<Home />} />
+            <Container>
+                <Routes>
+                    <Route path="/" element={<Home />} />
 
-                <Route path="*" element={<Home />} />
-            </Routes>
+                    <Route path="*" element={<Home />} />
+                </Routes>
+            </Container>
         </>
     )
 }
