4a077e3e19744036a9466f4275414c24cc5b8a35
Khaled Faour <khaledfaour2@gmail.com>
2022-07-18 11:47:00 +0300

    FRONTEND: Added Container Component

---
+++ frontend/src/components/container/container.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { Grid } from '@mui/material';
+
+const Container = ({children}) => {
+    return (
+        <Grid container justifyContent='center'>
+            <Grid item xs={12} md={10} lg={8}>
+                {children}
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Container;
