13471ea0d7760dff06e004dd8c57b9e84499127b
Khaled Faour <khaledfaour2@gmail.com>
2022-07-21 10:55:54 +0300

    BACKEND: Fixed addVote API Functionality

--- backend/controllers/Vote.Controller.js
+++ backend/controllers/Vote.Controller.js
@@ -1,10 +1,61 @@
 const Vote = require('../models/Vote.model');
+const Post = require('../models/Post.model');
 
 const addVote = async (req, res)=>{
+    console.log(req.body)
     try {
-        Vote.create({...req.body, user: req.user._id})
-        .then(async data=>{
-            res.status(201).json(data);
+        Vote.findOne({user: req.user._id, post_id: req.body.post_id}).then(async data=>{
+            if(data && req.body.type){
+                if(data.type === req.body.type){
+                        res.status(201).json(data);
+                }else{
+                    await Vote.findByIdAndUpdate(data._id, {type: req.body.type})
+                    if(req.body.type === 'down'){
+                        await Post.findByIdAndUpdate(req.body.post_id,{
+                            $push:{
+                                down_votes: data._id
+                            },
+                            $pull:{
+                                up_votes: data._id
+                            }
+                        });
+                    }else{
+                        await Post.findByIdAndUpdate(req.body.post_id,{
+                            $push:{
+                                up_votes: data._id
+                            },
+                            $pull:{
+                                down_votes: data._id
+                            }
+                        });
+                    }
+                    res.status(201).json(data);
+                }
+            
+            }else{
+                Vote.create({
+                    user: req.user._id,
+                    post_id: req.body.post_id,
+                    type: req.body.type ?? 'up'
+                }).then(async data=>{
+                    if(req.body.type === 'down'){
+                        await Post.findByIdAndUpdate(req.body.post_id,{
+                            $push:{
+                                down_votes: data._id
+                            }
+                        });
+                    }else{
+                        await Post.findByIdAndUpdate(req.body.post_id,{
+                            $push:{
+                                up_votes: data._id
+                            }
+                        });
+                    }
+                    
+                    res.status(201).json(data);
+                })
+                
+            }
         })
        
     } catch (error) {
