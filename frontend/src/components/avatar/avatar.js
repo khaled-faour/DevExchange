a3c702c5e0b04d7b4f4246c994d6c1ca31643d50
Khaled Faour <khaledfaour2@gmail.com>
2022-07-22 00:39:31 +0300

    FRONTEND: Fixed Avatar Component User profile_picrture URL Issue

--- frontend/src/components/avatar/avatar.js
+++ frontend/src/components/avatar/avatar.js
@@ -5,7 +5,15 @@ const Avatar = ({url, user}) => {
     const classes = styles();
     return (
         <div className={classes.avatar}>
-            {url ? <img src={url}/> : <div className={classes.avatarPlaceholder}>{user?.first_name[0] ?? "A"}{user?.last_name[0] ?? "B"}</div>}
+            {
+                url ? 
+                <img src={url}/> : 
+                user.profile_picture ?
+                    <img src={user.profile_picture}/> :
+                    <div className={classes.avatarPlaceholder}>
+                        {user?.first_name[0] ?? "A"}{user?.last_name[0] ?? "B"}
+                    </div>
+            }
         </div>
     );
 }
