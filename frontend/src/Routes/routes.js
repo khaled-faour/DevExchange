7acf21459428211b24090b8f7973bb36f211f1b8
Khaled Faour <khaledfaour2@gmail.com>
2022-07-21 22:02:26 +0300

    FRONTEND: Added Route to QuestionView Page

--- frontend/src/Routes/routes.js
+++ frontend/src/Routes/routes.js
@@ -3,6 +3,7 @@ import Home from "../pages/home/home";
 import Navbar from "../components/navbar/navbar";
 import Container from "../components/container/container";
 import Questions from "../pages/questions/questions";
+import QuestionView from "../pages/questionView/questionView";
 
 const Router = () => {
     return(
@@ -10,9 +11,13 @@ const Router = () => {
             <Navbar/>
             <Container>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/questions">
-                        <Route index element={<Questions />}/>
+                    {/* Home Screen Route */}
+                    <Route index element={<Home />} />
+
+                    {/* Question Routes */}
+                    <Route path="questions">
+                            <Route path="" element={<Questions />} />
+                            <Route path=":id" element={<QuestionView />} />
                     </Route>
                     <Route path="*" element={<Home />} />
                 </Routes>
