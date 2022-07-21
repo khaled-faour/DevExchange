b33040857bbea4b2b9c0d3552a550bc645b2c225
Khaled Faour <khaledfaour2@gmail.com>
2022-07-21 22:00:19 +0300

    FRONTEND: Added User Avatar to QuestionTile Component

--- frontend/src/components/questionTile/questionTile.js
+++ frontend/src/components/questionTile/questionTile.js
@@ -3,6 +3,8 @@ import styles from './styles';
 import Tag from '../tag/tag';
 import colors from '../../assets/styles/colors'
 import {useNavigate} from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import Avatar from '../../components/avatar/avatar';
 
 const QuestionTile = ({
     id,
@@ -15,26 +17,30 @@ const QuestionTile = ({
 })=>{
     
     const classes = styles();
-
+    const auth = useAuth();
     const navigate = useNavigate();
 
     const handleNavigation = ()=>{
-        navigate(`/question/${id}`);
+        navigate(`/questions/${id}`);
     }
     return (
         <div className={classes.container} style={{borderColor: borderColor}} onClick={handleNavigation}>
-
-            <h3 className={classes.title}>{title}</h3>
-
-            <div className={classes.tags}>
-                <div>
-                    {tags.map((tag, index)=><Tag key={index} title={tag}/>)}
-                </div>
-                {answers.length > 0 && <Tag title='Answered' color={colors.success}/>}
+            <div className={classes.avatar}>
+                <Avatar url={auth.user.profile_picture} />
             </div>
+            <div className={classes.content}>
+                <h3 className={classes.title}>{title}</h3>
 
-            <div className={classes.footer}>
-                <span>Asked on {createdAt} — <strong>{createdBy}</strong></span>
+                <div className={classes.tags}>
+                    <div>
+                        {tags.map((tag, index)=><Tag key={index} title={tag}/>)}
+                    </div>
+                    {answers.length > 0 && <Tag title='Answered' color={colors.success}/>}
+                </div>
+
+                <div className={classes.footer}>
+                    <span>Asked on {createdAt} — <strong>{createdBy}</strong></span>
+                </div>
             </div>
         </div>
     )
--- frontend/src/components/questionTile/styles.js
+++ frontend/src/components/questionTile/styles.js
@@ -1,13 +1,8 @@
 import { createUseStyles } from "react-jss";
 
 const styles = createUseStyles({
-    container: {
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        justifyContent: 'center',
-        gap: '5px',
+    container:{
+        display: "flex",
         padding: '20px',
         border: '1px solid',
         borderRadius: '5px',
@@ -19,9 +14,27 @@ const styles = createUseStyles({
             boxShadow: '0px 2px 5px 0px rgb(20 18 18 / 25%)'
         }
     },
+    avatar:{
+        height: '100%',
+        // padding: '10px'
+        marginTop: '-10px',
+        paddingRight: '10px'
+    },
+    content: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        gap: '5px',
+        
+    },
     title:{
         padding: '0 0 10px 0',
         margin: 0,
+        display: 'flex',
+        alignItems: 'center',
+        gap: '10px',
 
     },
     tags:{
