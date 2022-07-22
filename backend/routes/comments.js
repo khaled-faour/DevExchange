f1e4eaeb87dff41d81446734cfa90e84dc2e0050
Khaled Faour <khaledfaour2@gmail.com>
2022-07-22 10:43:02 +0300

    BACKEND: Fixed Comment Routes

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -4,9 +4,9 @@ const commentController = require('../controllers/Comment.Controller');
 const isAdmin = require('../middlewares/admin.middleware');
 
 
-router.post("/", isAdmin, commentController.addComment);
+router.post("/", commentController.addComment);
 router.put("/:id", isAdmin, commentController.updateById);
-router.delete("/:id", isAdmin, commentController.deleteById);
+router.delete("/:id", commentController.deleteById);
 router.get("/", commentController.getAll);
 router.get("/:id", commentController.getById);
 
