bce8996c261d30359057cb2c7bbaaf0b7e90a07d
Khaled Faour <khaledfaour2@gmail.com>
2022-07-24 01:26:46 +0300

    FRONTEND: Removed Video Toolbar Option

--- frontend/src/components/richTextEditor/richTextEdit.js
+++ frontend/src/components/richTextEditor/richTextEdit.js
@@ -6,13 +6,13 @@ const Editor = ({onChange, value, readOnly = false}) => {
 
   return <RichTextEditor
     readOnly={readOnly}
-    style={{border: readOnly ? 'none' : '1px solid #ccc', width: '100%'}}
+    style={{border: readOnly ? 'none' : '1px solid #ccc', width: '100%', height: 'fit-content'}}
     value={value} 
     onChange={onChange} 
     controls={[
         ['bold', 'strike', 'italic', 'underline', 'clean'],
         ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
-        ['link', 'image', 'video', 'code', 'blockquote'],
+        ['link', 'image', 'code', 'blockquote'],
         ['unorderedList', 'orderedList'],
         ['alignCenter', 'alignLeft', 'alignRight']
       ]}
