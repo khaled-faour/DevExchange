39ff9a1460db3e490ff0ab1b565f5765456d101e
Khaled Faour <khaledfaour2@gmail.com>
2022-07-23 10:55:50 +0300

    FRONTEND: Fixed Container Component Padding

--- frontend/src/components/container/container.js
+++ frontend/src/components/container/container.js
@@ -3,8 +3,8 @@ import { Grid } from '@mui/material';
 
 const Container = ({children}) => {
     return (
-        <Grid container justifyContent='center'>
-            <Grid item xs={12} md={10} xl={8}>
+        <Grid container justifyContent='center' p={2}>
+            <Grid item xs={12} md={10} xl={8} >
                 {children}
             </Grid>
         </Grid>
