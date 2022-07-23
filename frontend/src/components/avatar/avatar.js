e24a76570bbd59aaa25723cf704cd8a6d2a60ca6
Khaled Faour <khaledfaour2@gmail.com>
2022-07-23 10:38:51 +0300

    FRONTEND: Fixed Avatar Component URL Check

--- frontend/src/components/avatar/avatar.js
+++ frontend/src/components/avatar/avatar.js
@@ -8,8 +8,8 @@ const Avatar = ({url, user}) => {
             {
                 url ? 
                 <img src={url}/> : 
-                user.profile_picture ?
-                    <img src={user.profile_picture}/> :
+                user?.profile_picture ?
+                    <img src={user?.profile_picture}/> :
                     <div className={classes.avatarPlaceholder}>
                         {user?.first_name[0] ?? "A"}{user?.last_name[0] ?? "B"}
                     </div>
