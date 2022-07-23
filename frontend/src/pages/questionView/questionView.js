3577d285aa0ddbb632da75e4b4286ccd2ad70c90
Khaled Faour <khaledfaour2@gmail.com>
2022-07-24 01:28:30 +0300

    FRONTEND: Fixed AddAnswer User Can Add Only One Answer

--- frontend/src/pages/questionView/questionView.js
+++ frontend/src/pages/questionView/questionView.js
@@ -3,23 +3,31 @@ import styles from './styles';
 import {useParams} from 'react-router-dom';
 import Post from '../../components/post/post';
 import axios from 'axios';
-
+import AddAnswer from '../../components/addAnswer/addAnswer';
+import useAuth from '../../hooks/useAuth';
 
 const QuestionView = (props) => {
     const classes = styles();
+    const auth = useAuth();
     const { id } = useParams();
-    const [post, setPost] = useState([]);
+    const [post, setPost] = useState({});
     const [loading, setLoading] = useState(true);
+    const [addAnswerOpen, setAddAnswerOpen] = useState(false);
 
     useEffect(() => {
         axios.get(`/posts/${id}`).then(res => {
             setPost(res.data);
             setLoading(false);
-            console.log(res.data)
         }).catch(err => {
             console.log(err);
         })
     }, [id]);
+
+    useEffect(() => {
+        console.log("can answer: ", (post?.answers?.find(answer=>answer.user._id === auth.user._id) === undefined))
+        console.log("found: ", post?.answers?.find(answer=>answer.user._id === auth.user._id))
+        setAddAnswerOpen((post?.answers?.find(answer=>answer.user._id === auth.user._id) === undefined))
+    }, [post]);
 
     return (
         loading ? "Loading" : 
@@ -30,6 +38,8 @@ const QuestionView = (props) => {
             .map(answer => (
                 <Post key={answer.id} post={answer} />
             ))}
+
+            {addAnswerOpen && <AddAnswer />}
         </div>
     );
 }
