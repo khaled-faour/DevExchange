7b496a2208a806fcf466a6b23e5bf4e97649098e
Khaled Faour <khaledfaour2@gmail.com>
2022-07-24 13:58:55 +0300

    FRONTEND: Imported TutorCard Component to Tutors Component

--- frontend/src/pages/tutors/styles.js
+++ frontend/src/pages/tutors/styles.js
@@ -1,6 +1,14 @@
 import { createUseStyles } from "react-jss";
 
 const styles = createUseStyles({
+    container:{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        gap: "1rem",
+    }
 });
 
 export default styles;
--- frontend/src/pages/tutors/tutors.js
+++ frontend/src/pages/tutors/tutors.js
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import styles from './styles';
+import TutorCard from '../../components/tutorCard/tutorCard';
+import axios from 'axios';
+
 const Tutors = () => {
     const classes = styles();
-    
+    const [tutors, setTutors] = useState([]);
+    useEffect(() => {
+        axios.get('/tutorDetails')
+        .then(res => {
+            console.log("Tutors: ", res.data)
+            setTutors(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }, []);
+
     return (
-        <div>
-            <h1>Tutors</h1>
+        <div className={classes.container}>
+            {tutors.map(tutor => {
+                return <TutorCard key={tutor.id} tutor={tutor} />
+            })}
+            
         </div>
     )
 }
