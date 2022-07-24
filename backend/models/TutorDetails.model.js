632da78eda835967872e76cddf82558cf303267e
Khaled Faour <khaledfaour2@gmail.com>
2022-07-24 11:07:37 +0300

    BACKEND: Fixed TutorDetails Model

--- backend/models/TutorDetails.model.js
+++ backend/models/TutorDetails.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const tutorDetailsSchema = new mongoose.Schema({
+    title:{type: String, required: true},
     hourly_rate: {type: Number, required: true},
     description: {type: String, required: true},
     tags: [{type: String, ref: 'tag'}],
@@ -8,8 +9,13 @@ const tutorDetailsSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
     },
-    github_url: {type: String},
-    linkedin_url: {type: String}
+    github_url: {type: String, required: false},
+    linkedin_url: {type: String, required: false},
+    reviews: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'review',
+        required: false
+    }]
 }, { timestamps: true });
 
 module.exports = mongoose.model("TutorDetails", tutorDetailsSchema);
