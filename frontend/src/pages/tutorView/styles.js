526021bb1990ba08f8c70131083befcec66e465a
Khaled Faour <khaledfaour2@gmail.com>
2022-07-24 19:44:08 +0300

    FRONTEND: Added Styles to Tutor View Component

--- frontend/src/pages/tutorView/styles.js
+++ frontend/src/pages/tutorView/styles.js
@@ -1,6 +1,39 @@
 import {createUseStyles} from 'react-jss';
 
 const styles = createUseStyles({
+    container:{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+        width: '100%',
+        height: '100%',
+        boxSizing: 'border-box',
+    },
+    about:{
+        display: 'flex',
+        gap: '1rem',
+        flexDirection: 'column',
+        border: '1px solid #cdcdcf',
+        borderRadius: '5px',
+        width: '100%',
+        height: '100%',
+        boxSizing: 'border-box',
+        padding: "1rem",
+    },
+    aboutTitle:{
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+        margin: "0",
+    },
+    aboutDescription:{
+        fontSize: "1rem",
+        margin: "0",
+        // overflow: 'hidden',
+        // display: '-webkit-box',
+        // '-webkit-box-orient': 'vertical',
+        // /* truncate to 4 lines */
+        // '-webkit-line-clamp': 2
+    }
 });
 
 export default styles;
