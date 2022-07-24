d2356a1bcfaf8dc50104d3faa143183149200efd
Khaled Faour <khaledfaour2@gmail.com>
2022-07-24 11:08:15 +0300

    BACKEND: Fixed tutorDetails Route Middleware Issue

--- backend/routes/tutorDetails.js
+++ backend/routes/tutorDetails.js
@@ -4,7 +4,7 @@ const tutorDetailsContorller = require('../controllers/TutorDetails.Controller')
 const isAdmin = require('../middlewares/admin.middleware');
 
 
-router.post("/", isAdmin, tutorDetailsContorller.addTutorDetails);
+router.post("/", tutorDetailsContorller.addTutorDetails);
 router.put("/:id", isAdmin, tutorDetailsContorller.updateById);
 router.delete("/:id", isAdmin, tutorDetailsContorller.deleteById);
 router.get("/", tutorDetailsContorller.getAll);
