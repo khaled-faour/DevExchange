6d81bcb2eb4b471c212ca1f293e4a94024046dd3
Khaled Faour <khaledfaour2@gmail.com>
2022-07-24 11:06:58 +0300

    BACKEND: Fixed reviews Route Middleware Issue

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -4,7 +4,7 @@ const reviewsController = require('../controllers/Review.Controller');
 const isAdmin = require('../middlewares/admin.middleware');
 
 
-router.post("/", isAdmin, reviewsController.addReview);
+router.post("/", reviewsController.addReview);
 router.put("/:id", isAdmin, reviewsController.updateById);
 router.delete("/:id", isAdmin, reviewsController.deleteById);
 router.get("/", reviewsController.getAll);
