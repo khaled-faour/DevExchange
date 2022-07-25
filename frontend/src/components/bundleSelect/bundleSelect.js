8bd78c54bd26bfd7042da907412aabe7d171e5a3
Khaled Faour <khaledfaour2@gmail.com>
2022-07-26 01:16:47 +0300

    FRONTEND: Added BundleSelect Component

---
+++ frontend/src/components/bundleSelect/bundleSelect.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import styles from './style';
+import BundleCard from '../bundleCard/bundleCard';
+
+
+// Material UI
+import {Grid} from '@mui/material';
+const BundleSelect = ({bundles, handleSelect, selected}) => {
+    const classes = styles();
+    
+    return (
+        <Grid container spacing={3}>
+            {bundles.map((bundle, index)=>
+                    <BundleCard 
+                        key={index} 
+                        bundle={bundle} 
+                        selected={selected === bundle._id}
+                        onClick={()=>handleSelect(bundle._id)}
+                    />
+            )}
+        </Grid>
+    )
+}
+
+export default BundleSelect;
