8baef5e77973200514ef95b75b031ad03f72f491
Khaled Faour <khaledfaour2@gmail.com>
2022-07-26 01:17:19 +0300

    FRONTEND: Added ConfirmPurchase Component

---
+++ frontend/src/components/confirmPurchase/confirmPurchase.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+
+const ConfirmPurchase = () => {
+    return (
+        <div>
+            <h2>Confirm to proceed</h2>
+        </div>
+    )
+}
+
+export default ConfirmPurchase;
