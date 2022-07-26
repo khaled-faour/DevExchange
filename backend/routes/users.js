d01b8f6a7d24d0106adf428776473f968efea9a0
Khaled Faour <khaledfaour2@gmail.com>
2022-07-26 15:15:22 +0300

    BACKEND: Fixed User getUserProfile Missing Route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/User.Controller');
 const isAdmin = require('../middlewares/admin.middleware');
 
 
+router.get("/me", userController.getUserProfile);
 router.get("/:id", userController.getById);
 router.put("/:id", userController.updateById);
 
