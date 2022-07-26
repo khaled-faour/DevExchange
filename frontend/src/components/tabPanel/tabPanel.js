1d3c4ca4eefded5125d7040b0a28da6dce39423f
Khaled Faour <khaledfaour2@gmail.com>
2022-07-26 09:43:37 +0300

    FRONTEND: Added TabPanel Component

---
+++ frontend/src/components/tabPanel/tabPanel.js
@@ -0,0 +1,21 @@
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <div style={{ padding: '16px' }}>
+            <p>{children}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  export default TabPanel;
