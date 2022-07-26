e31a0da10aef334fe3cd7e341c5617a8d233b699
Khaled Faour <khaledfaour2@gmail.com>
2022-07-26 14:47:07 +0300

    BACKEND: Fixed User updateById Missing Function

--- backend/controllers/User.Controller.js
+++ backend/controllers/User.Controller.js
@@ -30,52 +30,21 @@ const getById = async (req, res)=>{
     }
 }
 
-    // login: async (req, res)=>{
-    //     try {
-    //         User.findOne({email: req.body.email}).then(async (userData)=>{
-    //             // 
-    //             if(userData && (await bcrypt.compare(req.body.password, userData.password))){
-    //                 let cookie = await bcrypt.hash(COOKIE_KEY, 10);
-    //                 UserSession.create({
-    //                     user: userData._id,
-    //                     session: cookie
-    //                 })
-    //                 return res.cookie("devexchange", cookie).status(200).json({user: {
-    //                     _id: userData._id,
-    //                     first_name: userData.first_name,
-    //                     last_name: userData.last_name,
-    //                     email: userData.email
-    //                 }})
-    //             }
-    //             return res.status(200).json({user:false, message: "Invalid Credentials"})
-    //         })
-    //     } catch (error) {
-    //         console.log(error);
-    //         res.status(500).send(error)
-    //     }
-    // },
+const updateById = async (req, res)=>{
+    try {
+        User.findByIdAndUpdate(req.params.id, {...req.body})
+        .then(async data=>{
+            const upadatedData = await User.findById(req.params.id)
+            res.status(201).json(upadatedData);
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+}
 
-    // logout: async (req, res )=>{
-    //     res.clearCookie("devexchange").status(200);
-    //     console.log(req.headers)
-    //     const cookies = req.headers.cookie.split(";");
-    //     let cookieValue= null;
-    //     cookies.forEach(cookie=>{
-    //         if(cookie.split("=")[0].trim() === "devexchange"){
-    //             cookieValue = decodeURIComponent(cookie.split("=")[1].trim())
-    //         }
-    //     })
-
-    //     const session = await UserSession.findOneAndRemove({session: cookieValue})
-    //     .then(()=>{
-    //         console.log("Session Cleared!");
-    //         res.status(200).send("Session Cleared")
-    //     }).catch(err=>{
-    //         console.log(err)
-    //     })
-        
-    // }
 module.exports = {
     register,
-    getById
+    getById,
+    updateById
 }
