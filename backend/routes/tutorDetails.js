39fb7b70bc1cd7d04fbeec11f1c584ac54d6f0ae
Khaled Faour <khaledfaour2@gmail.com>
2022-07-27 14:03:08 +0300

    BACKEND: Added TutorDetails deleteAvailability Route

--- backend/routes/tutorDetails.js
+++ backend/routes/tutorDetails.js
@@ -7,6 +7,7 @@ const isAdmin = require('../middlewares/admin.middleware');
 router.post("/", tutorDetailsContorller.addTutorDetails);
 router.post("/availability", tutorDetailsContorller.addTutorAvailability);
 router.put("/:id", isAdmin, tutorDetailsContorller.updateById);
+router.delete("/availability", tutorDetailsContorller.deleteTutorAvailability);
 router.delete("/:id", isAdmin, tutorDetailsContorller.deleteById);
 router.get("/", tutorDetailsContorller.getAll);
 router.get("/:id", tutorDetailsContorller.getById);
