1f6a1683420a2ec7339cd678a08f04ca97ea665c
Khaled Faour <khaledfaour2@gmail.com>
2022-07-28 16:37:16 +0300

    FRONTEND: Added TopRatedTutors Component Styles

---
+++ frontend/src/components/topRatedTutors/styles.js
@@ -0,0 +1,13 @@
+import { createUseStyles } from "react-jss";
+
+const styles = createUseStyles({
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "1rem",
+    }
+})
+
+export default styles;
