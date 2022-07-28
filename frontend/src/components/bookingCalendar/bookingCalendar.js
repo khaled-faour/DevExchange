5b468be8c4239537dbd04f9306f8eb5a0a404885
Khaled Faour <khaledfaour2@gmail.com>
2022-07-28 07:26:34 +0300

    FRONTEND: Added Booking Calendar Functionality

--- frontend/src/components/bookingCalendar/bookingCalendar.js
+++ frontend/src/components/bookingCalendar/bookingCalendar.js
@@ -1,40 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ScheduleMeeting } from 'react-schedule-meeting';
 import colors from '../../assets/styles/colors';
 import './styles.css';
+import axios from 'axios';
+
 
 Date.prototype.addDays = function(days) {
-    var date = new Date(this.valueOf());
-    date.setDate(date.getDate() + days);
-    return date;
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
 }
 
 function getDates(start, end) {
-    const startDate = new Date(start)
-    const stopDate = new Date(end)
-    var dateArray = new Array();
-    var currentDate = startDate;
-    while (currentDate <= stopDate) {
-        dateArray.push(new Date (currentDate));
-        currentDate = currentDate.addDays(1);
-    }
-    return dateArray;
+  const startDate = new Date(start)
+  const stopDate = new Date(end)
+  var dateArray = new Array();
+  var currentDate = startDate;
+  while (currentDate <= stopDate) {
+      dateArray.push(new Date (currentDate));
+      currentDate = currentDate.addDays(1);
+  }
+  return dateArray;
 }
 
+const BookingCalendar = ({tutor, onClose}) => {
 
-const BookingCalendar = ({tutor}) => {
-
-    // SAVE ARRAY OF DATES BETWEEN TWO DATES
-    const availableTimeslots = tutor?.availability?.map(availability=>{
-      return getDates(availability.start_date, availability.end_date).map((date) => {
-           const value = {
-            id: date,
-            startTime: new Date(new Date(new Date(date)).setHours(availability.start_time.split(":")[0], 0, 0, 0)),
-            endTime: new Date(new Date(new Date(date)).setHours(availability.end_time.split(":")[0], 0, 0, 0)),
+      const availableTimeslots = tutor.availability.map(availability=>{
+        console.log("Availability:: ", availability)
+        return getDates(availability.start_time, availability.end_time).map(date=>{
+          const start_hour = (new Date(availability.start_time)).toTimeString().split(':')
+          const end_hour = (new Date(availability.end_time)).toTimeString().split(':')
+          return {
+            id: availability._id,
+            startTime: new Date(new Date(new Date(date)).setHours(start_hour[0], 0, 0, 0)),
+            endTime: new Date(new Date(new Date(date)).setHours(end_hour[0], 0, 0, 0)),
           }
-           return value;
         })
-    })
+      })
+      
+    const handleConfirmation = (time) => {
+        axios.post('/users/book', {time, tutor:{_id: tutor._id, hourly_rate: tutor.hourly_rate}})
+        .then(res => {
+            onClose()            
+        }).catch(err => {
+            console.log(err);
+        });
+    }
       
       return (
         <div>
@@ -43,7 +54,7 @@ const BookingCalendar = ({tutor}) => {
                 primaryColor={colors.primary}
                 eventDurationInMinutes={30}
                 availableTimeslots={availableTimeslots.flat()}
-                onStartTimeSelect={console.log}
+                onStartTimeSelect={handleConfirmation}
             />
         </div>
       );
