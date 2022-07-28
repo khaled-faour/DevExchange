732345e3cbc4f8e3c9560c6b5ecd5f8cd58b6959
Khaled Faour <khaledfaour2@gmail.com>
2022-07-28 09:37:33 +0300

    FRONTEND: Added User Calendar Component

---
+++ frontend/src/components/userCalendar/userCalendar.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from './styles';
+import colors from '../../assets/styles/colors';
+
+// Material UI
+import AddIcon from '@mui/icons-material/Add';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+
+const UserCalendar  = ({user})=>{
+    const [schedule, setSchedule] = useState([]);
+    const classes = styles();
+    
+    const handleDeleteSchedule = (id)=>{
+        axios.delete(`/schedules/${id}`).then(res=>{
+            console.log(res.data)
+            fetchSchedule()
+        })
+    }
+
+    const fetchSchedule = ()=>{
+        axios.get(`/schedules/me`).then(res => {
+            console.log(res.data)
+            setSchedule(res.data);
+        }
+        ).catch(err => {
+            console.log(err);
+        });
+    }
+    useEffect(()=>{
+        fetchSchedule();
+    }, [])
+
+    return (
+        <div className={classes.container}>
+                {schedule.map((schedule, index) => {
+                    const startTime = new Date(schedule.start_time)
+                    const endTime = new Date(schedule.end_time)
+                    return (
+                        <div className={classes.innerContainer} key={index}>
+                            <div className={classes.availabilityContainer}>
+                                <div>
+                                    <h3>Days: <span>{startTime.toDateString()}</span></h3>
+                                    <h4>Time: <span>{startTime.toLocaleTimeString()} — {endTime.toLocaleTimeString()}</span></h4>
+                                </div>
+                            </div>
+                            <div className={classes.actionButtons}>
+                                <RemoveCircleOutlineIcon style={{color: colors.error}} onClick={()=>handleDeleteSchedule(schedule._id)}/>
+                            </div>
+                        </div>
+                    )
+                })}
+        </div>
+    )
+}
+
+export default UserCalendar;
