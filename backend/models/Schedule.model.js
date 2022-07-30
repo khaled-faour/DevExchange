eccdeab1e0c9d3af6fd5b5cdbce4548a70be2322
Khaled Faour <khaledfaour2@gmail.com>
2022-07-30 22:17:32 +0300

    BACKEND: Added meeting_id Field to ScheduleSchema

--- backend/models/Schedule.model.js
+++ backend/models/Schedule.model.js
@@ -11,6 +11,7 @@ const scheduleSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
     },
+    meeting_id:{type: String, required: true},
   
 }, { timestamps: true });
 
