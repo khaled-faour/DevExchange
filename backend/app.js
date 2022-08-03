e92470a3b19e872c60a5e8384285a792f59fdfcc
Khaled Faour <khaledfaour2@gmail.com>
2022-08-03 15:26:21 +0300

    BACKEND: Fixed Image Upload Limit

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,9 @@ const app = express();
 // Auth Middleware
 const authMiddleware = require('./middlewares/auth.middleware')
 
-app.use(express.json());
+app.use(express.json({
+    limit: '50mb'
+}));
 app.use(cors({origin: '*'}))
 
 app.use(cookieParser(process.env.SESSION_KEY));
