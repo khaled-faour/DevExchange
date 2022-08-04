98d2634419ffb68a59af394bbf14ae5165f23e94
Khaled Faour <khaledfaour2@gmail.com>
2022-08-04 14:15:18 +0300

    FRONTEND: Cleaned BookingModal Component

--- frontend/src/components/bookingModal/bookingModal.js
+++ frontend/src/components/bookingModal/bookingModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Button from '../button/button';
 import BookingCalendar from '../bookingCalendar/bookingCalendar';
 import { ToastContainer, toast } from 'react-toastify';
