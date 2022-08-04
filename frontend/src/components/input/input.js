d35670c47f685de3ab33c0923215a10fd05c7d2d
Khaled Faour <khaledfaour2@gmail.com>
2022-08-04 14:22:56 +0300

    FRONTEND: Cleaned Input Component

--- frontend/src/components/input/input.js
+++ frontend/src/components/input/input.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './styles';
 
 const Input = ({ onChange, value, placeholder, type, name, error, label}) => {
